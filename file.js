5cf48000de617b364f5f161d5ba7d033782bb092
Fabian De Greef <u0143117@kuleuven.be>
2022-03-25 09:36:10 +0100

    change on main

--- file.js
+++ file.js
@@ -3,5 +3,6 @@ when an unknown printer took a galley of type and scrambled it to make a type sp
 also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets 
 containing Lorem Ipsum passages, and more recently with desktop publishing 
 software like Aldus PageMaker including versions of Lorem Ipsum`;
-
-console.log(sText);
+for (let i = 0; i < 5; i++) {
+  console.log(sText);
+}
